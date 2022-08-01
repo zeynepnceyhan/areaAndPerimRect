f78109cfc3ee8d62af6c2d70c2f3fdec29da28d8
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 16:10:51 +0300

    area and perimeter of a rectangle
    internship

---
+++ areaandperimofrec/areaandperimofrec/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shortSide, longSide, perimeter, area;
+            Console.WriteLine("enter the length of the short side: ");
+            shortSide = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the length of the long side: ");
+            longSide = Convert.ToInt32(Console.ReadLine());
+
+            perimeter = (shortSide + longSide) * 2;
+            area = shortSide * longSide;
+
+            Console.WriteLine("The perimeter of the rectangle: " + perimeter);
+            Console.WriteLine("The area of the rectangle: " + area);
+            Console.ReadLine();
+        }
+
+    }
+
+}
